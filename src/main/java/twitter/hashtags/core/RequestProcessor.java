b0d9084c4f8602e10ec6bb7609fb2c64baaeb86c
Karan Patel <karan.k.patel@oracle.com>
2019-12-01 01:41:32 +0530

    Split getTweets into multiple functions

--- src/main/java/twitter/hashtags/core/RequestProcessor.java
+++ src/main/java/twitter/hashtags/core/RequestProcessor.java
@@ -9,7 +9,6 @@ import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import twitter.hashtags.constants.TwitterConstants;
-import twitter.hashtags.exception.HttpConnectionException;
 import twitter.hashtags.exception.ValidationException;
 import twitter.hashtags.manager.HttpConnectionManager;
 import twitter.hashtags.models.Tweet;
@@ -31,33 +30,22 @@ public class RequestProcessor {
     private String twitterAccessToken;
 
 
-    public ArrayList getTweets(String expression, String resultType) throws HttpConnectionException, IOException {
-
+    public ArrayList getTweets(String expression, String resultType) throws IOException {
         validateInput(resultType);
+        JSONObject response = fetchResponse(expression, resultType);
+        return parseOutput(response);
+    }
 
+    private ArrayList parseOutput(JSONObject response) {
         ArrayList<Tweet> tweets = new ArrayList<>();
 
-        Map<String, String>queryParams = new HashMap<String, String>(){{
-            put(TwitterConstants.BASE_QUERY_PARAMETER,expression);
-            put(TwitterConstants.RESULT_TYPE_QUERY_PARAMETER, resultType);
-        }};
-
-        ArrayList<Integer> expectedResponseCodes = new ArrayList<Integer>(){{
-            add(HttpStatus.OK.value());
-        }};
-
-        JSONObject response  = httpConnectionManager
-                .buildUrl(TwitterConstants.BASE_URL,TwitterConstants.SEARCH_API_PATH,queryParams)
-                .buildRequest(twitterAccessToken, HttpMethod.GET.toString(), null)
-                .fetchJSONResponse(expectedResponseCodes);
-
-        if(!response.has("statuses"))
+        if (!response.has("statuses"))
             return tweets;
 
         JSONArray statusArray = response.getJSONArray("statuses");
 
         statusArray.forEach(item -> {
-            JSONObject obj = (JSONObject)item;              //org.json uses raw iterator, hence type conversion is required
+            JSONObject obj = (JSONObject) item;              //org.json uses raw iterator, hence type conversion is required
             Tweet tweet = new Tweet(obj.getString("text"));
             tweets.add(tweet);
         });
@@ -65,18 +53,35 @@ public class RequestProcessor {
         return tweets;
     }
 
-    private void validateInput(String resultType) throws ValidationException{
-        List<String> resultTypes = new ArrayList<String>(){{
-                add("mixed");
-                add("recent");
-                add("popular");
+    private JSONObject fetchResponse(String expression, String resultType) throws IOException {
+        Map<String, String> queryParams = new HashMap<String, String>() {{
+            put(TwitterConstants.BASE_QUERY_PARAMETER, expression);
+            put(TwitterConstants.RESULT_TYPE_QUERY_PARAMETER, resultType);
         }};
 
-        if(!resultTypes.contains(resultType))
+        ArrayList<Integer> expectedResponseCodes = new ArrayList<Integer>() {{
+            add(HttpStatus.OK.value());
+        }};
+
+        return httpConnectionManager
+                .buildUrl(TwitterConstants.BASE_URL, TwitterConstants.SEARCH_API_PATH, queryParams)
+                .buildRequest(twitterAccessToken, HttpMethod.GET.toString(), null)
+                .fetchJSONResponse(expectedResponseCodes);
+        
+    }
+
+    private void validateInput(String resultType) {
+        List<String> resultTypes = new ArrayList<String>() {{
+            add("mixed");
+            add("recent");
+            add("popular");
+        }};
+
+        if (!resultTypes.contains(resultType))
             throw new ValidationException(
                     String.format("Invalid resultType \'%s\'. " +
                             "Please provide one from following" +
-                            " : mixed,recent,popular",resultType));
+                            " : mixed,recent,popular", resultType));
     }
 
 }
